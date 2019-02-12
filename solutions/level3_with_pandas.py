fbd28ab0eb8b58e2ab0370e7bf9c1c56d34de7b2
Kimiko Suzuki <sksuzuki@email.unc.edu>
2019-02-12 15:40:45 -0500

    Updates

--- solutions/level3_with_pandas.py
+++ solutions/level3_with_pandas.py
@@ -1,23 +1,22 @@
 import pandas as pd
+from sys import argv
 
 def parse_bed(bed):
     df = pd.read_csv(bed, sep='\t', header=None)
-    df_p = df[df[5] == '+'].copy()
-    df_p[6] = df_p[2] - df[1]
-    chromo_names = get_chromosome_names(df_p)
+    df.columns = ['chromosome', 'start', 'stop', 'ID', 'score', 'strand']
+    df_p = df[df['strand'] == '+'].copy()
+    df_p['len'] = df_p['stop'] - df['start']
+    chromo_names = df_p['chromosome'].unique()
 
     df_top = []
     for contig in chromo_names:
-        df_contig = df_p[df_p[0] == contig]
-        df_top.append(df_contig.nlargest(5,6))
+        df_contig = df_p[df_p['chromosome'] == contig]
+        df_top.append(df_contig.nlargest(5,'len'))
     df_top = pd.concat(df_top)
 
     with open('results/3.txt', 'w') as result:
         result.write(str(sum(df_top.index)))
 
-def get_chromosome_names(df_p):
-    return df_p[0].unique()
-
 if __name__ == "__main__":
-    bed = 'C:/Users/sksuzuki/Documents/EXCISION/level_3/ENCFF239FSU.bed'
-    parse_bed(bed)
+    parse_bed(argv[1])
+    # parse_bed('../data/3/ENCFF239FSU.bed')
